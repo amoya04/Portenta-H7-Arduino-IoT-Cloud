06b8c32604d72992f1fd1cbcb62972b218f82ab0
amoya04 <71283021+amoya04@users.noreply.github.com>
2020-10-03 18:38:57 +0200

    Update thingProperties.h

--- thingProperties.h
+++ thingProperties.h
@@ -5,8 +5,8 @@
 
 const char THING_ID[] = "250a70ea-83d3-479f-af9c-730f53fe6b1a";
 
-const char SSID[]     = SECRET_SSID;    // Network SSID (name)
-const char PASS[]     = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
+char* SSID = SECRET_SSID;    // Network SSID (name)
+const char *PASS = SECRET_PASS;    // Network password (use for WPA, or use as key for WEP)
 
 void onSetAlarmChange();
 
